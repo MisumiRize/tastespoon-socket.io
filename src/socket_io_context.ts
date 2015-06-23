9ce73d7883f2e68b5b58e6c349133dd223ca9f07
Misumi Rize <r@ayase-e.li>
2015-06-24 00:31:06 +0900

    create boilerplate

---
+++ src/socket_io_context.ts
@@ -0,0 +1,7 @@
+/// <reference path="../node_modules/tastespoon/tastespoon.d.ts" />
+/// <reference path="../typings/tsd.d.ts" />
+
+import Context = require("tastespoon/lib/context");
+
+export default class SocketIOContext extends Context {
+}
---
+++ src/socket_io_resource.ts
@@ -0,0 +1,6 @@
+/// <reference path="../node_modules/tastespoon/tastespoon.d.ts" />
+
+import Resource = require("tastespoon/lib/resource");
+
+export default class SocketIOResource extends Resource {
+}
